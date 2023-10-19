0574080ef8afb0dcd2cefef582dc9e9f5ddcac9c
Aristotelis Triadafyllidis <Aris@hameds-mbp.home>
2023-10-19 20:12:18 +0100

    Add gpt client

---
+++ services/chatGPT/chatGPT.go
@@ -0,0 +1,52 @@
+package chatgpt
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+)
+
+const gptEndpoint = "https://api.openai.com/v1/engines/gpt-4/completions"
+
+type GPTClient struct {
+	ApiKey string
+}
+
+func (client *GPTClient) GenerateText(prompt string) (string, error) {
+	requestPayload := map[string]interface{}{
+		"prompt":     prompt,
+		"max_tokens": 150,
+	}
+
+	jsonData, err := json.Marshal(requestPayload)
+	if err != nil {
+		return "", err
+	}
+
+	req, err := http.NewRequest("POST", gptEndpoint, bytes.NewBuffer(jsonData))
+
+	if err != nil {
+		return "", err
+	}
+
+	req.Header.Set("Authorization", "Bearer "+client.ApiKey)
+	req.Header.Set("Content-Type", "application/json")
+
+	httpClient := &http.Client{}
+
+	resp, err := httpClient.Do(req)
+
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	var responsePayload map[string] interface{}
+
+	err = json.NewDecoder(resp.Body).Decode(&responsePayload)
+	if err != nil {
+		return "", err
+	}
+
+	return responsePayload["choices"][0].((map[string]interface{})["text"].(string), nil)
+}
